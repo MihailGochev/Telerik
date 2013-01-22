2c163058713fc6b2db0fa1a77214f94b00884396
Mihail Gochev <michael.gochev@gmail.com>
2013-01-22 20:53:37 +0200

    Upload

---
+++ TextFiles/InsertLineNumbers/InsertLineNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class InsertLineNumbers
+{
+    static void Main()
+    {
+        StreamReader inputFile = new StreamReader(@"..\..\input.txt");
+        StreamWriter outputFile = new StreamWriter(@"..\..\output.txt");
+        string input;
+
+        using (inputFile)
+        {
+            input = inputFile.ReadToEnd();
+        }
+
+        string[] lines = input.Split('\n');
+
+        using (outputFile)
+        {
+            for (int line = 0; line < lines.Length; line++)
+            {
+                outputFile.Write("{0}: {1}", line + 1, lines[line]);
+            }
+        }
+    }
+}
