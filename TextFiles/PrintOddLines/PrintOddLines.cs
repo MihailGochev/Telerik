091d0e3fe6879002ce6a285b6a7deffc6adffa49
Mihail Gochev <michael.gochev@gmail.com>
2013-01-22 21:11:44 +0200

    Upload

---
+++ TextFiles/PrintOddLines/PrintOddLines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class PrintOddLines
+{
+    static void Main()
+    {
+        StreamReader inputFile = new StreamReader(@"..\..\input.txt");
+        string input;
+
+        using (inputFile)
+        {
+            input = inputFile.ReadToEnd();
+        }
+
+        string[] lines = input.Split('\n');
+
+        for (int line = 0; line < lines.Length; line++)
+        {
+            if (line % 2 == 1)
+            {
+                Console.Write(lines[line]);
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
