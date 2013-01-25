0aec5dbb488cbad5f11f13d84840d6ad95ae3572
Mihail Gochev <michael.gochev@gmail.com>
2013-01-25 22:36:33 +0200

    Upload

--- Methods/SubtractAndMultiplyPolynomial/SubtractAndMultiplyPolynomial.cs
+++ Methods/SubtractAndMultiplyPolynomial/SubtractAndMultiplyPolynomial.cs
@@ -11,22 +11,23 @@ class SubtractAndMultiplyPolynomial
 
     static void Main()
     {
-        int[] firstPolynomial = new int[] { 5, 0, 3 };
+        int[] firstPolynomial = new int[] { 5, 2, 3 };
         int[] secondPolynomial = new int[] { 3, 5, 1 };
 
 
         int[] result = PolynomialSubtraction(firstPolynomial, secondPolynomial);
 
-        Console.Write("({0}x² + {1}x + {2}) + ", firstPolynomial[2], firstPolynomial[1], firstPolynomial[0]);
+        Console.Write("({0}x² + {1}x + {2}) - ", firstPolynomial[2], firstPolynomial[1], firstPolynomial[0]);
         Console.Write("({0}x² + {1}x + {2}) = ", secondPolynomial[2], secondPolynomial[1], secondPolynomial[0]);
         Console.WriteLine("({0}x² + {1}x + {2})", result[2], result[1], result[0]);
 
-        int[] result2 = PolynomialMultiplication(firstPolynomial, secondPolynomial);
+        result = PolynomialMultiplication(firstPolynomial, secondPolynomial);
+
         Console.Write("({0}x² + {1}x + {2}) * ", firstPolynomial[2], firstPolynomial[1], firstPolynomial[0]);
         Console.Write("({0}x² + {1}x + {2}) = ", secondPolynomial[2], secondPolynomial[1], secondPolynomial[0]);
 
-        Console.WriteLine("({0}x4 + {3}x3 + {2}x² + {1}x + {2})", result2[4], result2[3], result2[2], result2[1], result2[0]);
-    }
+        Console.WriteLine("({0}x4 + {1}x3 + {2}x² + {3}x + {4})", result[4], result[3], result[2], result[1], result[0]);
+    } 
 
     static int[] PolynomialSubtraction(int[] first, int[] second)
     {
@@ -43,6 +44,7 @@ class SubtractAndMultiplyPolynomial
     static int[] PolynomialMultiplication(int[] first, int[] second)
     {
         int[] result = new int[first.Length * 2];
+
 
         for (int index = 0; index < first.Length; index++)
         {
