16433b49db0d55b6ba57067bad278eb9c968b884
Mihail Gochev <michael.gochev@gmail.com>
2013-01-28 16:40:24 +0200

    Upload

---
+++ Exams/Telerik Academy Exam 2 @ 7 Feb 2012/MessageInABottle/MessageInABottle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class MessageInABottle
+{
+    static int numberLenght = 4;
+
+    static void Main()
+    {
+
+        int maxNumber = 1 << (numberLenght);
+
+        maxNumber /= 2;
+
+        Console.WriteLine(maxNumber);
+
+        for (int currentCombination = 0; currentCombination < maxNumber; currentCombination++)
+        {
+            Console.WriteLine(Convert.ToString(currentCombination,2).PadLeft(numberLenght,'0'));
+        }
+    }
+
+    static int[] ReturnCombinations(string number)
+    {
+        int[] result = new int[numberLenght];
+        int nuberIndex;
+
+
+        return result;
+    }
+}
