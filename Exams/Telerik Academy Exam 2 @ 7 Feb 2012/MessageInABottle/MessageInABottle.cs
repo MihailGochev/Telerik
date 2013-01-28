957f2d778995b27caebd1609a0c722f6b4486ef0
Mihail Gochev <michael.gochev@gmail.com>
2013-01-28 17:03:31 +0200

    Upload

--- Exams/Telerik Academy Exam 2 @ 7 Feb 2012/MessageInABottle/MessageInABottle.cs
+++ Exams/Telerik Academy Exam 2 @ 7 Feb 2012/MessageInABottle/MessageInABottle.cs
@@ -3,29 +3,65 @@ using System.Linq;
 
 class MessageInABottle
 {
-    static int numberLenght = 4;
 
     static void Main()
     {
+        int numberLenght = 4;
+        string code = "ABCD";
+        int maxNumber = 1 << (numberLenght - 1);
 
-        int maxNumber = 1 << (numberLenght);
-
-        maxNumber /= 2;
-
-        Console.WriteLine(maxNumber);
 
         for (int currentCombination = 0; currentCombination < maxNumber; currentCombination++)
         {
-            Console.WriteLine(Convert.ToString(currentCombination,2).PadLeft(numberLenght,'0'));
+            Console.WriteLine();
+            Console.WriteLine(Convert.ToString(currentCombination, 2).PadLeft(numberLenght, '0'));
+            int[] result = ReturnCombinations(Convert.ToString(currentCombination, 2).PadLeft(numberLenght, '0'));
+
+
+
+            int index = 0;
+            int startIndex = 0;
+
+            while (result[index] > 0)
+            {
+                Console.WriteLine(result[index]);
+                Console.WriteLine(code.Substring(index, result[index]));
+                if (result[index] == 1)
+                {
+                    startIndex += result[index] + 1;
+
+                }
+                else
+                {
+                    startIndex += result[index];
+                }
+                index++;
+            }
         }
     }
 
     static int[] ReturnCombinations(string number)
     {
-        int[] result = new int[numberLenght];
-        int nuberIndex;
+        int[] result = new int[number.Length + 1];
+        int numberIndex = 0;
+        int lenght = 1;
+        char currentDigit = number[0];
 
-
+        for (int index = 1; index < number.Length; index++)
+        {
+            if (currentDigit == number[index])
+            {
+                lenght++;
+            }
+            else
+            {
+                currentDigit = number[index];
+                result[numberIndex++] = lenght;
+                lenght = 1;
+            }
+        }
+        result[numberIndex++] = lenght;
+        result[numberIndex] = -1;
         return result;
     }
 }
